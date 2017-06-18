a60ca69aefabc9484c055f47e4674a0e3029e738
David Maurer <davemaurermail@gmail.com>
2017-06-18 15:42:55 -0600

    refactor bubble_sort.rb in sorting_turing to break out long method.

--- sorting_turing/bubble_sort/bubble_sort.rb
+++ sorting_turing/bubble_sort/bubble_sort.rb
@@ -1,4 +1,7 @@
 class BubbleSort
+
+# *** Using an until with condition set inside of an each.
+
   # def sort(coll)
   #   done = false
   #   until done
@@ -15,18 +18,28 @@ class BubbleSort
   #   coll
   # end
 
+# *** Using do loops only. No need to conditions but iterates whether needed or not.
+
   def sort(coll)
     coll.length.times do
-      index   = 0
-      (coll.length - 1).times do
-        prev    = coll[index]
-        current = coll[index + 1]
-        if current < prev
-          coll[index], coll[index + 1] = coll[index + 1], coll[index]
-        end
-        index += 1
-      end
+      traverse(coll)
     end
     coll
+  end
+
+  def traverse(coll)
+    index = 0
+    (coll.length - 1).times do
+      swap(coll, index)
+      index += 1
+    end
+  end
+
+  def swap(coll, index)
+    prev = coll[index]
+    current = coll[index + 1]
+    if current < prev
+      coll[index], coll[index + 1] = coll[index + 1], coll[index]
+    end
   end
 end
