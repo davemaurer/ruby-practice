82e4ec383dff8e0c58422cadd7f9b3c847d81a55
David Maurer <davemaurermail@gmail.com>
2017-06-18 14:50:34 -0600

    commit changes sorting exercises

--- sorting_turing/bubble_sort/bubble_sort.rb
+++ sorting_turing/bubble_sort/bubble_sort.rb
@@ -1,17 +1,31 @@
 class BubbleSort
+  # def sort(coll)
+  #   done = false
+  #   until done
+  #     done = true
+  #     coll.each_with_index do |_, index|
+  #       prev = coll[index]
+  #       current = coll[index + 1]
+  #       if current && current < prev
+  #         coll[index], coll[index + 1] = coll[index + 1], coll[index]
+  #         done = false
+  #       end
+  #     end
+  #   end
+  #   coll
+  # end
+
   def sort(coll)
-    done = false
-    until done
-      done = true
-      coll.each_with_index do |_, index|
-        prev = coll[index]
-        current = coll[index + 1]
+    coll.length.times do
+      index   = 0
+      prev    = coll[index]
+      current = coll[index + 1]
+      (coll.length - 1).times do
         if current && current < prev
           coll[index], coll[index + 1] = coll[index + 1], coll[index]
-          done = false
         end
+        index += 1
       end
     end
-    coll
   end
 end
