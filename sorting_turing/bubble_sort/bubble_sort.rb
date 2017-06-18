a4b5580143f0d17341060f608ebf9e65c19ac0f4
David Maurer <davemaurermail@gmail.com>
2017-06-18 14:37:34 -0600

    add tests for sorting more than one element. Use a loop with conditional to prevent unnecessary iterations if last few elements are sorted.

--- sorting_turing/bubble_sort/bubble_sort.rb
+++ sorting_turing/bubble_sort/bubble_sort.rb
@@ -1,11 +1,17 @@
 class BubbleSort
   def sort(coll)
-    coll.each_with_index do |_, index|
-      prev = coll[index]
-      current = coll[index + 1]
-      if current && current < prev
-        coll[index], coll[index + 1] = coll[index + 1], coll[index]
+    done = false
+    until done
+      done = true
+      coll.each_with_index do |_, index|
+        prev = coll[index]
+        current = coll[index + 1]
+        if current && current < prev
+          coll[index], coll[index + 1] = coll[index + 1], coll[index]
+          done = false
+        end
       end
     end
+    coll
   end
 end
--- sorting_turing/bubble_sort/bubble_sort_test.rb
+++ sorting_turing/bubble_sort/bubble_sort_test.rb
@@ -3,7 +3,19 @@ require './bubble_sort'
 describe BubbleSort do
   it "sorts two values" do
     bs = BubbleSort.new
-    result = bs.sort([2,1])
-    expect(result).to eq([1,2])
+    result = bs.sort([2, 1])
+    expect(result).to eq([1, 2])
+  end
+
+  it "sorts two values that are already in order" do
+    bs = BubbleSort.new
+    result = bs.sort([1, 2])
+    expect(result).to eq([1, 2])
+  end
+
+  it "sorts more than two values" do
+    bs = BubbleSort.new
+    result = bs.sort([1, 3, 5, 2, 4])
+    expect(result).to eq([1, 2, 3, 4, 5])
   end
 end
