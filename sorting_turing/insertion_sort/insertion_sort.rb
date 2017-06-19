c25e212a0434dee410bb804b3a86fed08c699721
David Maurer <davemaurermail@gmail.com>
2017-06-18 19:20:11 -0600

    make first test pass for insertion sort in sorting_turing

--- sorting_turing/insertion_sort/insertion_sort.rb
+++ sorting_turing/insertion_sort/insertion_sort.rb
@@ -1,3 +1,21 @@
 class InsertionSort
-  
+  def sort(coll)
+    sorted = [coll.shift]
+    coll.each do |element|
+      i = 0
+      insert_element(sorted, element, i)
+    end
+    sorted
+  end
+
+  def insert_element(collection, num, i)
+    if num <= collection[i]
+      collection.insert(i, num)
+    elsif i == collection.length - 1
+      collection << num
+    else
+      insert_element(collection, num, i + 1)
+    end
+    collection
+  end
 end
