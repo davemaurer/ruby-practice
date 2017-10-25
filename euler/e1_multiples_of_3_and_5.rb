d03bed203b09743f75b23a3b58f7214f3d8de508
David Maurer <davemaurermail@gmail.com>
2017-10-25 14:37:44 -0600

    add faster solution to euler e_1

--- euler/e1_multiples_of_3_and_5.rb
+++ euler/e1_multiples_of_3_and_5.rb
@@ -17,9 +17,18 @@ Benchmark.bm do |bm|
       multiples.reduce(:+)
     end
   end
+  bm.report do
+    iterations.times do
+      total = 0
+      (1..1000).each { |num| total += num if num % 3 == 0 || num % 5 == 0 }
+      total
+    end
+  end
 end
 
 # Benchmark results
 # user system total real
 # 22.280000 0.110000 22.390000 (22.987829) - Slick one line way
 # 19.070000 0.100000 19.170000 (19.274996) - Using .each
+# 13.350000   0.060000  13.410000 ( 13.463234) - Using .each without an array. No reduce needed.
+
