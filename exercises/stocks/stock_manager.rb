79af6b235c6abf6430e76edfc70a7a1bcf2fc58b
David Maurer <davemaurermail@gmail.com>
2017-12-28 20:44:17 -0700

    two tests passing.

--- exercises/stocks/stock_manager.rb
+++ exercises/stocks/stock_manager.rb
@@ -7,7 +7,13 @@ Write a method that will calculate the best time to buy, and then sell the stock
 Constraints: Stock must be bought before selling. Stock cannot be bought and sold in the same minute(index).
 =end
 
-prices = [12, 4, 6, 9, 6, 8]
+class StockManager
+  def find_buy_points(prices)
+    prices[prices.index(prices.min)..-1]
+  end
 
-best_profit()
-
+  def find_best_profit(prices)
+    min = find_buy_points(prices)[0]
+    find_buy_points(prices).max - min
+  end
+end
--- exercises/stocks/stock_manager_spec.rb
+++ exercises/stocks/stock_manager_spec.rb
@@ -5,6 +5,15 @@ describe StockManager do
     sm = StockManager.new
     prices = [12, 4, 6, 9, 6, 8]
     result = sm.find_buy_points(prices)
-    expect(result[0]).to eq 4
+
+    expect(result).to eq [4, 6, 9, 6, 8]
+  end
+
+  it "finds the best profit margin" do
+    sm = StockManager.new
+    prices = [12, 4, 6, 9, 6, 8]
+    result = sm.find_best_profit(prices)
+
+    expect(result).to eq 5
   end
 end
