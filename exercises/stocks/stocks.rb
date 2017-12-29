3ba261155f0bcfa0b71d9f87df258e6da45ee37c
David Maurer <davemaurermail@gmail.com>
2017-12-28 20:02:28 -0700

    add exercise for stock profit algorithm.

---
+++ exercises/stocks/stocks.rb
@@ -0,0 +1,13 @@
+=begin
+Given an array of stock prices where each element in the array is an integer representing the price in dollars, and
+the index of each element represents the time in minutes past 8am:
+
+Write a method that will calculate the best time to buy, and then sell the stock in order to make the most profit.
+
+Constraints: Stock must be bought before selling. Stock cannot be bought and sold in the same minute(index).
+=end
+
+prices = [12, 4, 6, 9, 6, 8]
+
+best_profit()
+
