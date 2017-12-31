06b9c38a9be1b4980c3b4e8f4a60bd52f9c432b7
David Maurer <davemaurermail@gmail.com>
2017-12-30 18:47:06 -0700

    for exercises/product_of_other_indexes straight forward solution using abstracted Ruby methods.

--- exercises/product_of_other_indexes/product.rb
+++ exercises/product_of_other_indexes/product.rb
@@ -3,4 +3,12 @@ class Product
   def initialize(nums)
     @numbers = nums
   end
+
+  def find_products_excluding_current_element
+    @numbers.map.with_index do |_, index|
+      other_nums = @numbers.dup
+      other_nums.delete_at(index)
+      other_nums.reduce(:*)
+    end
+  end
 end
--- exercises/product_of_other_indexes/product_spec.rb
+++ exercises/product_of_other_indexes/product_spec.rb
@@ -6,4 +6,8 @@ describe Product do
   it "takes takes in an array of numbers" do
     expect(product.numbers).to eq [4, 3, 1, 2]
   end
+
+  it "can get the product of all elements except current element" do
+    expect(product.find_products_excluding_current_element).to eq [6, 8, 24, 12]
+  end
 end
