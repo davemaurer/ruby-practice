af592896a0a1b627c9d1cbe0b5c35ed93456a2a4
David Maurer <davemaurermail@gmail.com>
2017-12-30 18:50:34 -0700

    for exercises/product_of_other_indexes solves if zeros are involved.

--- exercises/product_of_other_indexes/product_spec.rb
+++ exercises/product_of_other_indexes/product_spec.rb
@@ -7,7 +7,12 @@ describe Product do
     expect(product.numbers).to eq [4, 3, 1, 2]
   end
 
-  it "can get the product of all elements except current element" do
+  it "gets the product of all elements except current element" do
     expect(product.find_products_excluding_current_element).to eq [6, 8, 24, 12]
+  end
+
+  it "gets the product of all elements expect current element if zeros are included" do
+    product = Product.new([4, 0, 1, 2])
+    expect(product.find_products_excluding_current_element).to eq [0, 8, 0, 0]
   end
 end
